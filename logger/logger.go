e305e0da6edf141da7c38cd31274088513aaa099
Aynakeya <aynakeya@aynakeya.com>
2022-07-19 20:34:11 -0700

    fix logger

--- logger/logger.go
+++ logger/logger.go
@@ -20,15 +20,15 @@ func init() {
 		NoColors:    true,
 	})
 	file, err := os.OpenFile(config.Log.Path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		Logger.Out = io.MultiWriter(file, os.Stdout)
+	} else {
+		Logger.Info("Failed to log to file, using default stdout")
+	}
 	if config.Log.RedirectStderr {
 		Logger.Info("panic/stderr redirect to log file")
 		if _, err = paniclog.RedirectStderr(file); err != nil {
 			Logger.Infof("Failed to redirect stderr to to file: %s", err)
 		}
-	}
-	if err == nil {
-		Logger.Out = io.MultiWriter(file, os.Stdout)
-	} else {
-		Logger.Info("Failed to log to file, using default stdout")
 	}
 }
