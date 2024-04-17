6ef4ba3fe813d0b7fb1b0d8ccaa8332571fc4c1d
aynakeya <aynakeya.official@gmail.com>
2024-04-17 11:47:28 -0700

    fix windows gethandle

--- gui/xfyne/window_windows.go
+++ gui/xfyne/window_windows.go
@@ -3,12 +3,15 @@
 
 package xfyne
 
-import "fyne.io/fyne/v2"
+import (
+	"fyne.io/fyne/v2"
+	"unsafe"
+)
 
 func GetWindowHandle(window fyne.Window) uintptr {
 	glfwWindow := getGlfwWindow(window)
 	if glfwWindow == nil {
 		return 0
 	}
-	return uintptr(glfwWindow.GetWin32Window())
+	return uintptr(unsafe.Pointer(glfwWindow.GetWin32Window()))
 }
